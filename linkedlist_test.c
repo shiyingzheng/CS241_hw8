4faf868327d4bf9fad26214bcaa6c204664176af
BEN <bstern+github@oberlin.edu>
2013-11-16 14:36:17 -0600

    added blanklines and stuff

--- linkedlist_test.c
+++ linkedlist_test.c
@@ -5,6 +5,8 @@
 int main(){
 	int n=50;
 	int q;
+
+	//test addend and removeend
 	linkedlist* list=linkedlist_init(sizeof(int));
 	for(int i=0;i<n;i++){
 		q=i+n;
@@ -17,6 +19,9 @@ int main(){
 	}
 	printf("\n");
 	linkedlist_free(list);
+
+
+	//test the iterator
 	list=linkedlist_init(sizeof(int));
 	for(int i=0;i<n;i++){
 		q=i+n;
@@ -29,6 +34,9 @@ int main(){
 	printf("\n");
 	linkedlist_free(list);
 	linkedlist_freeiter(iter);
+
+
+	//test the getfront and getend
 	list=linkedlist_init(sizeof(char*));
 	char* string="This is a string";
 	char* string2="meow";
